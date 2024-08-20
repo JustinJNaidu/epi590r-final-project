a1d516f4b25d95478b92b572592af5b2f9ef08a6
Justin Naidu <JustinJNaidu@gmail.com>
2024-08-20 19:36:44 -0400

    Created function for standard deviation calculations

--- code/Functions.R
+++ code/Functions.R
@@ -1,14 +1,39 @@
+library(tidyverse)
+
 ss_clean <- read_csv(here::here("data", "clean", "ss_clean.csv"))
 
-ss_clean <- data %>%
+ss_clean <- ss_clean %>%
 	rename(
-		`Person ID` = Person.ID,
-		`Duration of Sleep` = Sleep.Duration,
-		`Quality of Sleep` = Quality.of.Sleep,
-		`Physical Activity Level` = Physical.Activity.Level,
-		`Stress Level` = Stress.Level,
-		`BMI Category` = BMI.Category,
-		`Heart Rate` = Heart.Rate,
-		`Daily Steps` = Daily.Steps,
-		`Sleep Disorder` = Sleep.Disorder
+		Person_ID = `Person ID`,
+		Sleep_Duration = `Sleep Duration`,
+		Sleep_Quality = `Quality of Sleep`,
+		Physical_Activity_Level = `Physical Activity Level`,
+		Stress_Level = `Stress Level`,
+		BMI_Category = `BMI Category`,
+		Heart_Rate = `Heart Rate`,
+		Daily_Steps = `Daily Steps`,
+		Sleep_Disorder = `Sleep Disorder`
 	)
+
+
+# Define the custom function to calculate standard deviation
+custom_sd <- function(x) {
+	# Calculate the standard deviation
+	result <- sd(x, na.rm = TRUE)
+	return(result)
+}
+
+# Calculate the standard deviation of Sleep_Duration
+sleep_duration_sd <- custom_sd(ss_clean$Sleep_Duration)
+
+# Print the result
+print(sleep_duration_sd)
+
+
+# Calculate standard deviations for other variables
+stress_level_sd <- custom_sd(ss_clean$Stress_Level)
+heart_rate_sd <- custom_sd(ss_clean$Heart_Rate)
+
+# Print the results
+print(stress_level_sd)
+print(heart_rate_sd)
